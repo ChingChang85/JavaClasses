1b623b423638d4e9467f98b5b55115a5d4bde86d
ChingChang <kenny850207@gmail.com>
2017-07-28 09:03:29 +0800

    170727

--- P002/src/Helloer.java
+++ P002/src/Helloer.java
@@ -9,8 +9,13 @@
  */
 
 public class Helloer {
-
+	
 	public static void main(String[] args) {
+		Helloer helloer = new Helloer();
+		helloer.sayHello();
+	}
+
+	public void sayHello() {
 		System.out.println("I¡¦m a helloer.");
 	}
 }
